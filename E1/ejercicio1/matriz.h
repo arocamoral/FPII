6998bf16e702c497789a2d0b41afd69e32f12b2d
Alejandro Rocamora <alex.rt.2500@gmail.com>
2022-03-30 00:07:36 +0200

    30/03-0:07

--- E1/ejercicio1/matriz.h
+++ E1/ejercicio1/matriz.h
@@ -12,23 +12,19 @@ private:
     int **M;
     int NF,NC;
 public:
+    ~Matriz(){
+        DEL();
+    }
     Matriz(){
         NF=0;
         NC=0;
         M = NULL;
     }
-    Matriz(const Matriz& A) {
-        NF=A.NF;
-        NC=A.NC;
+    Matriz(Matriz& P){
+        NF=P.NF;
+        NC=P.NC;
         M = new int* [NF];
-        for(int i=0;i<NF;i++){
-            M[i] = new int[NC];
-        }
-        for(int i=0;i<NF;i++){
-            for(int j=0;j<NC;j++){
-                M[i][j] = A.M[i][j];
-            }
-        }
+        asignarmemoria(P);
     }
     Matriz(int N, int n){
         NF = N;
@@ -43,35 +39,16 @@ public:
             }
         }
     }
-    ~Matriz() {
-        for(int i=0;i<NF;i++){
-            delete [] M[i];
-        }
-        delete [] M;
-    }
-    friend ostream& operator << (ostream& medio, const Matriz& A){
-        if(A.NC==0 && A.NF==0){
-            medio << "MATRIZ NULA" << endl;
-        }else{
-        for(int i=0;i<A.NF;i++){
-            for(int j=0;j<A.NC;j++){
-                medio << A.M[i][j] << "  ";
-            }
-            medio << endl;
-            medio << endl;
-        }
-        }
-        return medio;
-    }
-    Matriz operator =(const Matriz& P){
-        for(int i=0;i<NF;i++){
-            delete [] M[i];
-        }
-        delete [] M;
-        NF=P.NF;
-        NC=P.NC;
 
-        M = new int* [NF];
+    //////////      //////////      //////////
+
+    void DEL(){
+        for(int i=0;i<NF;i++){
+            delete [] M[i];
+        }
+        delete [] M;
+    }
+    void asignarmemoria(Matriz& P){
         for(int i=0;i<NF;i++){
             M[i] = new int[NC];
         }
@@ -80,8 +57,56 @@ public:
                 M[i][j] = P.M[i][j];
             }
         }
+    }
+
+    //////////      //////////      //////////
+
+    friend ostream& operator << (ostream& medio, const Matriz& P){
+        if(P.NC==0 && P.NF==0){
+            medio << "MATRIZ NULA" << endl;
+        }else{
+        for(int i=0;i<P.NF;i++){
+            for(int j=0;j<P.NC;j++){
+                medio << P.M[i][j] << "  ";
+            }
+            medio << endl;
+            medio << endl;
+        }
+        }
+        return medio;
+    }
+    Matriz operator =(Matriz& P){
+        DEL();
+        NF=P.NF;
+        NC=P.NC;
+        M = new int* [NF];
+        asignarmemoria(P);
         return (*this);
     }
+    Matriz operator +(const Matriz& P){
+        Matriz res(NF,NC);
+        if(NF==P.NF && NC==P.NC){
+            for(int i=0;i<NF;i++){
+                res.M[NF][NC]=M[NF][NC]+P.M[NF][NC];
+            }
+        }else{
+            cout << "NO SUMA" << endl;
+        }
+        return res;
+    }
+//    Matriz operator -(){
+//    }
+//    Matriz operator *(){
+//    }
+//    Matriz operator (){
+//    }
+//    Matriz operator (){
+//    }
+//    Matriz operator (){
+//    }
 };
 
 #endif // MATRIZ_H
+
+
+//para que sirve const
